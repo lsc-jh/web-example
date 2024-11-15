2a27512ea9811513bca1919efde8b68aa4a82716
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-15 17:03:53 +0100

    added separate pages

---
+++ app/mi-2/page.tsx
@@ -0,0 +1,24 @@
+import {CalcRes, PageProps} from "@/common-lib/types";
+
+export default async function Home({ searchParams }: PageProps) {
+  const { a, b, operator = "+" } = searchParams;
+  console.log(a, b, operator);
+
+  const result = await fetch(
+    `http://localhost:8000/calculate/${a}/${b}?operator=${encodeURIComponent(operator || "+")}`,
+  );
+
+  const message: CalcRes = await result.json();
+
+  return (
+    <div>
+      <h1 className="text-lg font-bold mb-2">MI-2</h1>
+      <hr />
+      <br />
+      <p>a: {message.a}</p>
+      <p>b: {message.b}</p>
+      <p>operator: {message.operator}</p>
+      <p>result: {message.result}</p>
+    </div>
+  );
+}
