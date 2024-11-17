a4733baf59e3b4766c0a8b913e5352623b59d53e
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-17 17:56:04 +0100

    added number input components

--- app/mi-1/page.tsx
+++ app/mi-1/page.tsx
@@ -1,12 +1,25 @@
 import {CalcRes, PageProps} from "@/common-lib/types";
+import NumberInputs from "@/app/mi-1/number-inputs";
 
-export default async function Home({ searchParams }: PageProps) {
+export default async function Home(props: PageProps) {
+  const searchParams = await props.searchParams;
   const
     { a, b, operator = "+" } = searchParams;
-  console.log(a, b, operator);
+
+  if(isNaN(a) || isNaN(b)) {
+    return (
+      <div>
+        <h1 className="text-lg font-bold mb-2">MI-1</h1>
+        <hr />
+        <NumberInputs />
+        <br />
+        <p>Invalid input</p>
+      </div>
+    );
+  }
 
   const result = await fetch(
-    `http://localhost:8000/calculate/${a}/${b}?operator=${encodeURIComponent(operator || "+")}`,
+    `http://localhost:8000/calculate/${a}/${b}?operator=%2B`,
   );
 
   const message: CalcRes = await result.json();
@@ -15,6 +28,7 @@ export default async function Home({ searchParams }: PageProps) {
     <div>
       <h1 className="text-lg font-bold mb-2">MI-1</h1>
       <hr />
+      <NumberInputs />
       <br />
       <p>a: {message.a}</p>
       <p>b: {message.b}</p>
